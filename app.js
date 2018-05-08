5356b5b470abc8c20d41b3fa3cc2894ea391319f
Camara Dienaba <34746425+Dienaba@users.noreply.github.com>
2018-05-08 14:58:28 +0200

    Supprime users

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var formulaire = require('./routes/formulaire');
 var benevole = require('./routes/benevole');
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/formulaire', formulaire);
 app.use('/benevole', benevole);
 
