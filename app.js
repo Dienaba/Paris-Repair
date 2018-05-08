c852d9a76fc1f133efde081802852dc8c93ecd10
Camara Dienaba <34746425+Dienaba@users.noreply.github.com>
2018-05-08 14:24:11 +0200

    Création route et view bénévole et formulaire inscription

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var formulaire = require('./routes/formulaire');
+var benevole = require('./routes/benevole');
 
 var app = express();
 
@@ -21,6 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/formulaire', formulaire);
+app.use('/benevole', benevole);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/benevole.js
+++ routes/benevole.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Paris Repair' });
+  res.render('benevole', { title: 'Paris Repair' });
 });
 
 module.exports = router;
